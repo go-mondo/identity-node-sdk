b990b8a0532d11783f9bfadfd8358fe17c7540f6
Nate Iler <nateiler@gmail.com>
2025-10-30 22:05:09 -0600

    fix: url schemas

--- src/common/schema/url.ts
+++ src/common/schema/url.ts
@@ -1,3 +1,5 @@
+import * as z from 'zod/v4';
+
 export function appendSearchParams(
   path: string,
   params: Map<string, string | undefined | null> | URLSearchParams
@@ -12,3 +14,17 @@ export function appendSearchParams(
 
   return [path, vals.filter(Boolean).join('&')].filter(Boolean).join('?');
 }
+
+const UrlObjectScheama = z
+  .url()
+  .pipe(z.transform((url) => new URL(url)))
+  .or(z.instanceof(URL));
+export type UrlObjectInput = z.input<typeof UrlObjectScheama>;
+export type UrlObject = z.output<typeof UrlObjectScheama>;
+
+const UrlStringScheama = z
+  .instanceof(URL)
+  .pipe(z.transform((url) => url.toString()))
+  .or(z.url());
+export type UrlStringInput = z.input<typeof UrlStringScheama>;
+export type UrlString = z.output<typeof UrlStringScheama>;
