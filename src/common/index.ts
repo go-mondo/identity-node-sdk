dc94ffacf7dd24f29df03fce93ccf93e0838ea96
Nate Iler <nateiler@gmail.com>
2026-05-08 14:02:57 -0600

    fix: exports

--- src/common/index.ts
+++ src/common/index.ts
@@ -10,7 +10,10 @@ export * from './errors/http.js';
 export * from './errors/validation.js';
 export type { MondoInstance } from './resources/init.js';
 
-export type { Authorizer } from './resources/authorization.js';
+export type {
+  AccessTokenProvider,
+  Authorizer,
+} from './resources/authorization.js';
 export * from './schema/schema.js';
 
 export default MondoIdentity;
